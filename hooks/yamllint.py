59dcc6a97557ec6a8ddd3b8174bd93bda7c5ec40
ɯ̹t͡ɕʲi <u@chimata.org>
2020-01-18 09:35:27 +0900

    fix a yamllint hook bug when mulitple files are given
    - 引数にコミット対象のすべてのファイルが与えられることがわかってなかった

--- hooks/yamllint.py
+++ hooks/yamllint.py
@@ -4,18 +4,15 @@ import os
 import subprocess
 import sys
 
-args = ' '.join([f"\"{x}\"" for x in sys.argv[1:-2]])
-file_path = sys.argv[-1]
-
 yamllint_exe = 'yamllint'
 
 if 'PATHEXT' in os.environ:
     exts = os.environ['PATHEXT'].split(os.pathsep)
     possible_exe_names = tuple(
-        'yamllint' + ext.lower() for ext in exts
-    ) + ('yamllint',)
+        yamllint_exe + ext.lower() for ext in exts
+    ) + (yamllint_exe,)
 else:
-    possible_exe_names = ('yamllint',)
+    possible_exe_names = (yamllint_exe,)
 
 for path in os.environ.get('PATH', '').split(os.pathsep):
     for possible_exe_name in possible_exe_names:
@@ -23,6 +20,18 @@ for path in os.environ.get('PATH', '').split(os.pathsep):
         if os.path.isfile(joined) and os.access(joined, os.X_OK):
             yamllint_exe = joined
 
-subprocess.call(f"echo {file_path}")
-sys.exit(subprocess.call(
-    f"{yamllint_exe} - {args} < \"{file_path}\"", shell=True))
+files = []
+args = []
+
+for x in sys.argv[1:]:
+    if os.path.isfile(x):
+        files += [f"\"{x}\""]
+    else:
+        args += [x]
+
+args = ' '.join(args)
+
+for f in files:
+    subprocess.call(f"echo {f}")
+    sys.exit(subprocess.call(
+        f"{yamllint_exe} - {args} < {f}", shell=True))
