de6b001d27317cf4327f6a2367f88a0541d66b73
ɯ̹t͡ɕʲi <u@chimata.org>
2020-01-18 09:43:27 +0900

    fix installing environment bug
    - Installing environment がうまく行かない不具合修正
      - pyproject.tomlの tool.poetry.scripts に run_yamllint として実行ファイルを登録し、yamllintのentryに指定することで、Installing environment時に実行ファイルがビルドされpre-commit runから呼び出される

--- hooks/yamllint.py
+++ hooks/yamllint.py
@@ -4,34 +4,40 @@ import os
 import subprocess
 import sys
 
-yamllint_exe = 'yamllint'
 
-if 'PATHEXT' in os.environ:
-    exts = os.environ['PATHEXT'].split(os.pathsep)
-    possible_exe_names = tuple(
-        yamllint_exe + ext.lower() for ext in exts
-    ) + (yamllint_exe,)
-else:
-    possible_exe_names = (yamllint_exe,)
+def run():
+    yamllint_exe = 'yamllint'
 
-for path in os.environ.get('PATH', '').split(os.pathsep):
-    for possible_exe_name in possible_exe_names:
-        joined = os.path.join(path, possible_exe_name)
-        if os.path.isfile(joined) and os.access(joined, os.X_OK):
-            yamllint_exe = joined
-
-files = []
-args = []
-
-for x in sys.argv[1:]:
-    if os.path.isfile(x):
-        files += [f"\"{x}\""]
+    if 'PATHEXT' in os.environ:
+        exts = os.environ['PATHEXT'].split(os.pathsep)
+        possible_exe_names = tuple(
+            yamllint_exe + ext.lower() for ext in exts
+        ) + (yamllint_exe,)
     else:
-        args += [x]
+        possible_exe_names = (yamllint_exe,)
 
-args = ' '.join(args)
+    for path in os.environ.get('PATH', '').split(os.pathsep):
+        for possible_exe_name in possible_exe_names:
+            joined = os.path.join(path, possible_exe_name)
+            if os.path.isfile(joined) and os.access(joined, os.X_OK):
+                yamllint_exe = joined
 
-for f in files:
-    subprocess.call(f"echo {f}")
-    sys.exit(subprocess.call(
-        f"{yamllint_exe} - {args} < {f}", shell=True))
+    files = []
+    args = []
+
+    for x in sys.argv[1:]:
+        if os.path.isfile(x):
+            files += [f"\"{x}\""]
+        else:
+            args += [x]
+
+    args = ' '.join(args)
+
+    for f in files:
+        subprocess.call(f"echo {f}")
+        sys.exit(subprocess.call(
+            f"{yamllint_exe} - {args} < {f}", shell=True))
+
+
+if __name__ == '__main__':
+    run()
